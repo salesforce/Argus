e8274787a28816ee55b067f036963b5fa3ec236d
jbhatt <jbhatt@salesforce.com>
2016-04-14 16:32:57 -0700

    Adding methods

--- WardenClient/src/main/java/com/salesforce/dva/warden/client/DefaultWardenClient.java
+++ WardenClient/src/main/java/com/salesforce/dva/warden/client/DefaultWardenClient.java
@@ -9,7 +9,10 @@ public abstract class DefaultWardenClient implements WardenClient {
 	
 	// This is how the client talks to the server.
 	private WardenClientService clientService;
-
+	public DefaultWardenClient()
+	{
+		clientService = new DefaultWardenClientService();
+	}
 	@Override
 	public void register(List <Policy> policies, int port) {
 		clientService.register(policies, port);
