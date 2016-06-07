35cb99366277db73859a18e303800da71f944828
Raj Sarkapally <rsarkapally@salesforce.com>
2016-06-07 12:49:36 -0700

    Distributed scheduling with database

---
+++ ArgusCore/src/main/java/com/salesforce/dva/argus/service/DistributedSchedulingLockService.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016, Salesforce.com, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Salesforce.com nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.salesforce.dva.argus.service;
+
+import com.salesforce.dva.argus.entity.DistributedSchedulingLock;
+import com.salesforce.dva.argus.service.GlobalInterlockService.LockType;
+
+/**
+ * Provides methods to update and retrieve the distributed scheduling lock record.
+ * 
+ * @author Raj Sarkapally rsarkapally@salesforce.com
+ *
+ */
+public interface DistributedSchedulingLockService extends Service{
+
+	 /**
+     * Updates and returns the distributed schedule lock object for a given schedule type.
+     *
+     * @param   type        The scheduling type represented as a long value.
+     * @param   jobsBlockSize   The no of jobs each scheduler schedules as a block.
+	 * @param   schedulingRefreshInterval The time in millis scheduler refresh jobs
+     
+     * @return  Updated distributed schedule object
+     */
+	
+	DistributedSchedulingLock updateNGetDistributedScheduleByType(LockType type,int jobsDequeBlockSize, long schedulingRefreshInterval);
+}
