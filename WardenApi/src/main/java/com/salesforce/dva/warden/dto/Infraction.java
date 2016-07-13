bd54807ef3c7d838e3eebbcefc529ea8e8532e16
jbhatt <jbhatt@salesforce.com>
2016-07-13 15:16:24 -0700

    Adding hashCode and equals methods for some of the dtos

--- WardenApi/src/main/java/com/salesforce/dva/warden/dto/Infraction.java
+++ WardenApi/src/main/java/com/salesforce/dva/warden/dto/Infraction.java
@@ -11,7 +11,7 @@ import java.math.BigInteger;
 public class Infraction
     extends Entity
 {
-    private BigInteger policy_id;
+	private BigInteger policy_id;
     private BigInteger user_id;
     private Long infraction_timestamp;
     private Long expiration_timestamp;
@@ -55,6 +55,50 @@ public class Infraction
     {
         this.expiration_timestamp = expiration_timestamp;
     }
+    
+    @Override
+   	public int hashCode() {
+   		final int prime = 31;
+   		int result = 1;
+   		result = prime * result + ((expiration_timestamp == null) ? 0 : expiration_timestamp.hashCode());
+   		result = prime * result + ((infraction_timestamp == null) ? 0 : infraction_timestamp.hashCode());
+   		result = prime * result + ((policy_id == null) ? 0 : policy_id.hashCode());
+   		result = prime * result + ((user_id == null) ? 0 : user_id.hashCode());
+   		return result;
+   	}
+
+   	@Override
+   	public boolean equals(Object obj) {
+   		if (this == obj)
+   			return true;
+   		if (obj == null)
+   			return false;
+   		if (getClass() != obj.getClass())
+   			return false;
+   		Infraction other = (Infraction) obj;
+   		if (expiration_timestamp == null) {
+   			if (other.expiration_timestamp != null)
+   				return false;
+   		} else if (!expiration_timestamp.equals(other.expiration_timestamp))
+   			return false;
+   		if (infraction_timestamp == null) {
+   			if (other.infraction_timestamp != null)
+   				return false;
+   		} else if (!infraction_timestamp.equals(other.infraction_timestamp))
+   			return false;
+   		if (policy_id == null) {
+   			if (other.policy_id != null)
+   				return false;
+   		} else if (!policy_id.equals(other.policy_id))
+   			return false;
+   		if (user_id == null) {
+   			if (other.user_id != null)
+   				return false;
+   		} else if (!user_id.equals(other.user_id))
+   			return false;
+   		return true;
+   	}
+
 
     @Override
     public Object createExample(  )
--- WardenApi/src/main/java/com/salesforce/dva/warden/dto/SuspensionLevel.java
+++ WardenApi/src/main/java/com/salesforce/dva/warden/dto/SuspensionLevel.java
@@ -50,8 +50,52 @@ public class SuspensionLevel
     {
         this.suspension_time = suspension_time;
     }
+    
 
     @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((infraction_count == null) ? 0 : infraction_count.hashCode());
+		result = prime * result + ((level_number == null) ? 0 : level_number.hashCode());
+		result = prime * result + ((policy_id == null) ? 0 : policy_id.hashCode());
+		result = prime * result + ((suspension_time == null) ? 0 : suspension_time.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SuspensionLevel other = (SuspensionLevel) obj;
+		if (infraction_count == null) {
+			if (other.infraction_count != null)
+				return false;
+		} else if (!infraction_count.equals(other.infraction_count))
+			return false;
+		if (level_number == null) {
+			if (other.level_number != null)
+				return false;
+		} else if (!level_number.equals(other.level_number))
+			return false;
+		if (policy_id == null) {
+			if (other.policy_id != null)
+				return false;
+		} else if (!policy_id.equals(other.policy_id))
+			return false;
+		if (suspension_time == null) {
+			if (other.suspension_time != null)
+				return false;
+		} else if (!suspension_time.equals(other.suspension_time))
+			return false;
+		return true;
+	}
+
+	@Override
     public Object createExample(  )
     {
         SuspensionLevel result = new SuspensionLevel(  );
