1499a48d5eb250b35011077e3f40cbebc1dd688e
Chris Nardi <cnardi-dev@users.noreply.github.com>
2016-07-27 15:13:53 -0700

    Breadcrumbs (#78)
    * update config section to display better.
    
    * add autofocus directive to focus an input on page load.
    
    * small updates to help normalize templates spacing and alignment, headings, tables, & other minor clean up.
    
    * updates for new DVA services status dashboard.
    
    * remove unused rootScope from controller.
    
    * add support for parameterization fields to DVA service status dashboard. factor out method to augment expression with controls data to the dashboard service. updateexisting charts to use refactored method.
    
    * update serverice status db row layout for better responsiveness and slightly longer indicator label size.
    
    * add support for a slightly larger service text label.
    
    * setup for loading spinner for charts. keep 'loading' text for small charts. refactor small chart options and indicator status rendering.
    
    * add breadcrumbs site navigation. add filter for first letter of string to uppercase.
    
    * fix console error when logging out on the dashboard list. Use Auth service and check for null for remoteUser.
    
    * fix url path identification for breadcrumbs links to work outstide local environment. Remove some vertical spacing to bring page content higher.
    
    * fix console error showing due to null chart.
    
    * update breadcrumbs to icon-based in-line style. Display for 2nd level only, hide top level.
    
    * update top nav to show current active tab, including similar routes in same tab.

---
+++ ArgusWeb/app/utils/filters.js
@@ -0,0 +1,9 @@
+angular.module('filters', [])
+		.filter('capitalize', function() {
+				return function(input, scope) {
+						if (input != null) {
+								input = input.toLowerCase();
+								return input.substring(0,1).toUpperCase() + input.substring(1);
+						}
+				}
+		});
