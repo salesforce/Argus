dac917bac9cece208a0c34b2cc249dab0792817f
Shouvik Mani <shouvik.mani@shouvikma-ltm.internal.salesforce.com>
2016-08-11 08:54:10 -0700

    Service and controller for expression autocomplete

--- ArgusWeb/app/js/argus.js
+++ ArgusWeb/app/js/argus.js
@@ -1,8 +1,8 @@
 /*! Copyright (c) 2016, Salesforce.com, Inc.
  * All rights reserved.
- *  
+ *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- *   
+ *
  *      Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  *
  *      Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
@@ -80,6 +80,7 @@ angular.module('argus.services', [
   'argus.services.metrics',
   'argus.services.namespace',
   'argus.services.notifications',
+  'argus.services.search',
   'argus.services.storage',
   'argus.services.tags',
   'argus.services.triggers',
