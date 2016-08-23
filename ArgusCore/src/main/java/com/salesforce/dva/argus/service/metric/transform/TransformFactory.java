616ff6e8cf60992320796ae8c6dc621a007b95cc
Shouvik Mani <shouvikm@cmu.edu>
2016-08-23 15:50:28 -0700

    RPCA Seasonal Anomaly Detection (#98)
    * RPCA transform initial
    
    * Add matrix operations and standardization. Current working implementation
    
    * Add back RPCA unit tests
    
    * Parse season length constant and calculate season frequency
    
    * Improvements to code style and clarity. Exceptions for edge cases.
    
    * Improvements to code style and clarity. Exceptions for edge cases.

--- ArgusCore/src/main/java/com/salesforce/dva/argus/service/metric/transform/TransformFactory.java
+++ ArgusCore/src/main/java/com/salesforce/dva/argus/service/metric/transform/TransformFactory.java
@@ -180,6 +180,8 @@ public class TransformFactory {
                 return new AnomalyDetectionGaussianZScoreTransform();
             case ANOMALY_KMEANS:
                 return new AnomalyDetectionKMeansTransform();
+            case ANOMALY_RPCA:
+                return new AnomalyDetectionRPCATransform();
             default:
                 throw new UnsupportedOperationException(functionName);
         } // end switch
@@ -249,7 +251,8 @@ public class TransformFactory {
         GROUPBY("GROUPBY", "Creates groups of metrics based on some matching criteria and then performs the given aggregation."),
         ANOMALY_DENSITY("ANOMALY_DENSITY", "Calculates an anomaly score (0-100) for each value of the metric based on the probability density of each value with a Gaussian distribution."),
         ANOMALY_ZSCORE("ANOMALY_ZSCORE", "Calculates an anomaly score (0-100) for each value of the metric based on the z-score of each value with a Gaussian distribution."),
-        ANOMALY_KMEANS("ANOMALY_KMEANS", "Calculates an anomaly score (0-100) for each value of the metric based on a K-means clustering of the metric data.");
+        ANOMALY_KMEANS("ANOMALY_KMEANS", "Calculates an anomaly score (0-100) for each value of the metric based on a K-means clustering of the metric data."),
+        ANOMALY_RPCA("ANOMALY_RPCA", "Calculates an anomaly score (0-100) for each value of the metric based on the RPCA matrix decomposition algorithm.");
 
         private final String _name;
         private final String _description;
