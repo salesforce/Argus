f3bb3dd4c4aef203c41d424191927473da63569b
Pimeng Fu <pfu@salesforce.com>
2016-09-02 14:50:08 -0700

    add urlParams feature to the develop, some debug to be done

--- ArgusWeb/app/js/directives/controls/text.js
+++ ArgusWeb/app/js/directives/controls/text.js
@@ -1,5 +1,5 @@
 angular.module('argus.directives.controls.text', [])
-.directive('agText', ['CONFIG', function(CONFIG) {
+.directive('agText', ['$routeParams', 'CONFIG', function($routeParams, CONFIG) {
     return {
         restrict: 'EA',
         scope: {
@@ -7,13 +7,32 @@ angular.module('argus.directives.controls.text', [])
             labelName: '@label',
             controlValue: '@default'
         },
-        require: '^agDashboard',
-        //templateUrl:  CONFIG.templatePath + 'argus-text-control.html',
-        template: '<B>{{labelName}} : </B> <input type="text" ng-model="controlValue">',
+        // require: '^agDashboard',
+        // //templateUrl:  CONFIG.templatePath + 'argus-text-control.html',
+        // template: '<B>{{labelName}} : </B> <input type="text" ng-model="controlValue">',
+        // link: function(scope, element, attributes, dashboardCtrl) {
+        //     dashboardCtrl.updateControl(scope.controlName, scope.controlValue, "agText");
+        //     scope.$watch('controlValue', function(newValue, oldValue) {
+        //         dashboardCtrl.updateControl(scope.controlName, newValue, "agText");
+        //     });
+        // }
+
+        controller: function($scope) {
+            $scope.ctrlVal = $scope.controlValue;
+
+            // check $routeParams to override controlValue
+            for (var prop in $routeParams) {
+                if (prop == $scope.controlName) {
+                    $scope.ctrlVal = $routeParams[prop];
+                }
+            };
+        },
+        require:'^agDashboard',
+        template:'<B>{{labelName}} : </B> <input type="text" ng-model="ctrlVal">',
         link: function(scope, element, attributes, dashboardCtrl) {
-            dashboardCtrl.updateControl(scope.controlName, scope.controlValue, "agText");
-            scope.$watch('controlValue', function(newValue, oldValue) {
-                dashboardCtrl.updateControl(scope.controlName, newValue, "agText");
+            dashboardCtrl.updateControl(scope.controlName, scope.ctrlVal, "agText");
+            scope.$watch('ctrlVal', function(newValue, oldValue){
+                dashboardCtrl.updateControl(scope.controlName, newValue, "agText", true);
             });
         }
     }
