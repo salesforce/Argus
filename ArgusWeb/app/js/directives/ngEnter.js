89032b42e598864a71c23a46cdd0f8f83c9e66da
John Xu <liuxizi.xu@salesforce.com>
2016-09-06 10:45:18 -0700

    allow metrics expression to be entered with enter key

---
+++ ArgusWeb/app/js/directives/ngEnter.js
@@ -0,0 +1,19 @@
+/**
+ * Created by liuxizi.xu on 9/6/16.
+ */
+'use strict';
+
+angular.module('argus.directives')
+.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
