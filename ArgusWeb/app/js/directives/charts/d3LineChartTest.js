dbca20ca27eab90962b572c0511b748e6c733c56
Pimeng Fu <pfu@salesforce.com>
2016-10-21 18:09:21 -0700

    add resize feature, brush status will remain after resize

--- ArgusWeb/app/js/directives/charts/d3LineChartTest.js
+++ ArgusWeb/app/js/directives/charts/d3LineChartTest.js
@@ -16,13 +16,32 @@ angular.module('argus.directives.charts.d3LineChartTest', [])
             link: function(scope, element, attrs) {
                 var currSeries = attrs.series;
                 // Layout parameters
-                var margin = {top: 20, right: 20, bottom: 450, left: 40};
-                var margin2 = {top: 500, right: 20, bottom: 100, left: 0};
+                var containerHeight = 500;
+                var containerWidth = element.parent().width();
+                var mainChartRatio = 0.6 //ratio of height
+                    , tipBoxRatio = 0.2
+                    , brushChartRatio = 0.2;
+                var marginTop = 20,
+                    marginBottom = 100,
+                    marginLeft = 40,
+                    marginRight = 20;
+
+                var width = containerWidth - marginLeft - marginRight;
+                var height = parseInt((containerHeight - marginTop - marginBottom) * mainChartRatio);
+                var height2 = parseInt((containerHeight - marginTop - marginBottom) * brushChartRatio);
+                var margin = {top: marginTop,
+                              right: marginRight,
+                              bottom: containerHeight - marginTop - height,
+                              left: marginLeft};
+
+                var margin2 = {top: containerHeight - height2 - marginBottom,
+                               right: marginRight,
+                               bottom: marginBottom,
+                               left: marginLeft};
+
+
                 var tipPadding = 6;
                 var crossLineTipPadding = 2;
-                var width = element.parent().width() - margin.left - margin.right;
-                var height = 800 - margin.top - margin.bottom;
-                var height2 = 800 - margin2.top - margin2.bottom;
 
                 // Local helpers
                 var bisectDate = d3.bisector(function(d) { return d[0]; }).left;
@@ -30,175 +49,195 @@ angular.module('argus.directives.charts.d3LineChartTest', [])
                 var formatValue = d3.format(',');
                 var tooltipCreator = function() {};
 
-                // Base graph setup
-                var x = d3.scaleTime().range([0, width]);
-                var x2 = d3.scaleTime().range([0, width]); //for brush
-                var y = d3.scaleLinear().range([height, 0]);
-                var y2 = d3.scaleLinear().range([height2, 0]);
-                var z = d3.scaleOrdinal().range(d3.schemeCategory10);
-                var nGridX = 10;
-                var nGridY = 10;
-                //Axis
-                var xAxis = d3.axisBottom()
+
+
+                //graph setup variables
+                var x, x2, y, y2, z,
+                    nGridX = 10, nGridY = 10,
+                    xAxis, xAxis2, yAxis, xGrid, yGrid,
+                    line, line2,
+                    brush, zoom,
+                    svg, xAxisG, xAxisG2, yAxisG, xGridG, yGridG, //g
+                    focus, context, clip, brushG, chartRect, //g
+                    tip, tipBox, tipItems,
+                    crossline
+                    ;
+
+// Base graph setup ==========================================================>
+                function setGraph() {
+                    x = d3.scaleTime().range([0, width]);
+                    x2 = d3.scaleTime().range([0, width]); //for brush
+                    y = d3.scaleLinear().range([height, 0]);
+                    y2 = d3.scaleLinear().range([height2, 0]);
+                    z = d3.scaleOrdinal().range(d3.schemeCategory10);
+                    nGridX = 10;
+                    nGridY = 10;
+                    //Axis
+                    xAxis = d3.axisBottom()
                         .scale(x)
                         .ticks(nGridX)
                     ;
 
-                var xAxis2 = d3.axisBottom() //for brush
+                    xAxis2 = d3.axisBottom() //for brush
                         .scale(x2)
                         .ticks(nGridX);
 
-                var yAxis = d3.axisLeft()
+                    yAxis = d3.axisLeft()
                         .scale(y)
                         .ticks(nGridY)
                         .tickFormat(d3.format('.2s'))
                     ;
 
-                //grid
-                var xGrid = d3.axisBottom()
+                    //grid
+                    xGrid = d3.axisBottom()
                         .scale(x)
                         .ticks(nGridX)
                         .tickSizeInner(-height)
                     ;
 
-                var yGrid = d3.axisLeft()
+                    yGrid = d3.axisLeft()
                         .scale(y)
                         .ticks(nGridY)
                         .tickSizeInner(-width)
                     ;
 
-                //line
-                var line = d3.line()
-                    .x(function(d) {
-                        return x(d[0]);
-                    })
-                    .y(function(d) {
-                        return y(d[1]);
-                    });
-                //line2 (for brush area)
-                var line2 = d3.line()
-                    .x(function(d) {
-                        return x2(d[0]);
-                    })
-                    .y(function(d) {
-                        return y2(d[1]);
-                    });
+                    //line
+                    line = d3.line()
+                        .x(function (d) {
+                            return x(d[0]);
+                        })
+                        .y(function (d) {
+                            return y(d[1]);
+                        });
+                    //line2 (for brush area)
+                    line2 = d3.line()
+                        .x(function (d) {
+                            return x2(d[0]);
+                        })
+                        .y(function (d) {
+                            return y2(d[1]);
+                        });
 
 
-                //brush
-                var brush = d3.brushX()
-                    .extent([[0, 0], [width, height2]])
-                    .on("brush end", brushed);
+                    //brush
+                    brush = d3.brushX()
+                        .extent([[0, 0], [width, height2]])
+                        .on("brush end", brushed);
 
-                //zoom
-                var zoom = d3.zoom()
-                    .scaleExtent([1, Infinity])
-                    .translateExtent([[0, 0], [width, height]])
-                    .extent([[0, 0], [width, height]])
-                    .on("zoom", zoomed);
+                    //zoom
+                    zoom = d3.zoom()
+                        .scaleExtent([1, Infinity])
+                        .translateExtent([[0, 0], [width, height]])
+                        .extent([[0, 0], [width, height]])
+                        .on("zoom", zoomed);
 
-                //Add elements to SVG
-                var svg = d3.select(element[0]).append('svg')
+                    //Add elements to SVG
+                    svg = d3.select(element[0]).append('svg')
                         .attr('width', width + margin.left + margin.right)
                         .attr('height', height + margin.top + margin.bottom)
+                        .attr('id', 'svg')
                         .append('g')
                         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
                     ;
 
-                var xAxisG = svg.append('g')
-                    .attr('class', 'x axis')
-                    .attr('transform', 'translate(0,' + height + ')')
-                    .call(xAxis);
+                    xAxisG = svg.append('g')
+                        .attr('class', 'x axis')
+                        .attr('transform', 'translate(0,' + height + ')')
+                        .call(xAxis);
 
-                var yAxisG = svg.append('g')
-                    .attr('class', 'y axis')
-                    .call(yAxis);
+                    yAxisG = svg.append('g')
+                        .attr('class', 'y axis')
+                        .call(yAxis);
 
-                var xGridG = svg.append('g')
-                    .attr('class', 'x grid')
-                    .attr('transform', 'translate(0,' + height + ')')
-                    .call(xGrid);
+                    xGridG = svg.append('g')
+                        .attr('class', 'x grid')
+                        .attr('transform', 'translate(0,' + height + ')')
+                        .call(xGrid);
 
-                var yGridG = svg.append('g')
-                    .attr('class', 'y grid')
-                    .call(yGrid);
-
-
-                // Mouseover/tooltip setup
-                var focus = svg.append('g')
-                    .attr('class', 'focus')
-                    .style('display', 'none');
-                focus.append('circle')
-                    .attr('r', 4.5);
-
-                //Brush, zoom, pan
-                //clip path
-                svg.append("defs").append("clipPath")
-                    .attr("id", "clip")
-                    .append("rect")
-                    .attr("width", width)
-                    .attr("height", height);
-                //brush area
-                var context = svg.append("g")
-                    .attr("class", "context")
-                    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
-
-                //set brush area axis
-                context.append("g")
-                    .attr("class", "xBrush axis")
-                    .attr("transform", "translate(0," + height2 + ")")
-                    .call(xAxis2);
-
-                context.append("g")
-                    .attr("class", "brush")
-                    .call(brush)
-                    .call(brush.move, x.range()); //change the x axis range when brush area changes
-
-                //the graph rectangle area
-                svg.append('rect')
-                    .attr('class', 'chartOverlay')
-                    .attr('width', width)
-                    .attr('height', height)
-                    .on('mouseover', function() {
-                        focus.style('display', null);
-                    })
-                    .on('mouseout', function() {
-                        focus.style('display', 'none');
-                    })
-                    .on('mousemove', mousemove)
-                    .call(zoom)
-                ;
+                    yGridG = svg.append('g')
+                        .attr('class', 'y grid')
+                        .call(yGrid);
 
 
-                var reset = d3.select('#reset')
+                    // Mouseover/tooltip setup
+                    focus = svg.append('g')
+                        .attr('class', 'focus')
+                        .style('display', 'none');
+                    focus.append('circle')
+                        .attr('r', 4.5);
+
+                    //Brush, zoom, pan
+                    //clip path
+                    clip = svg.append("defs").append("clipPath")
+                        .attr("id", "clip")
+                        .append("rect")
+                        .attr("width", width)
+                        .attr("height", height);
+                    //brush area
+                    context = svg.append("g")
+                        .attr("class", "context")
+                        .attr("transform", "translate(0," + margin2.top + ")");
+
+                    //set brush area axis
+                    xAxisG2 = context.append("g")
+                        .attr("class", "xBrush axis")
+                        .attr("transform", "translate(0," + height2 + ")")
+                        .call(xAxis2);
+
+                    brushG = context.append("g")
+                        .attr("class", "brush")
+                        .call(brush)
+                        .call(brush.move, x.range()); //change the x axis range when brush area changes
+
+                    //the graph rectangle area
+                    chartRect = svg.append('rect')
+                        .attr('class', 'chartOverlay')
+                        .attr('width', width)
+                        .attr('height', height)
+                        .on('mouseover', function () {
+                            focus.style('display', null);
+                        })
+                        .on('mouseout', function () {
+                            focus.style('display', 'none');
+                        })
+                        .on('mousemove', mousemove)
+                        .call(zoom)
+                    ;
+
+                    tip = svg.append('g')
+                        .attr('class', 'legend');
+                    tipBox = tip.append('rect')
+                        .attr('rx', tipPadding)
+                        .attr('ry', tipPadding);
+                    tipItems = tip.append('g')
+                        .attr('class', 'legend-items');
+
+                    //focus tracking
+                    crossline = focus.append('g')
+                        .attr('id', 'crossline');
+                    crossline.append('line')
+                        .attr('id', 'crossLineX')
+                        .attr('class', 'crossLine');
+                    crossline.append('line')
+                        .attr('id', 'crossLineY')
+                        .attr('class', 'crossLine');
+                    crossline.append('text')
+                        .attr('id', 'crossLineTip');
+                }
+
+                setGraph();
+//graph set up done====================================================================>
+
+                //button set up
+                d3.select('#reset')
                     .on('click', reset);
 
-                var oneHour = d3.select('#oneHour')
+                d3.select('#oneHour')
                     .on('click', brushMinute(60));
 
-                var oneDay = d3.select('#oneDay')
+                d3.select('#oneDay')
                     .on('click', brushMinute(60*24));
-
-                var tip = svg.append('g')
-                    .attr('class', 'legend');
-                var tipBox = tip.append('rect')
-                    .attr('rx', tipPadding)
-                    .attr('ry', tipPadding);
-                var tipItems = tip.append('g')
-                    .attr('class', 'legend-items');
-
-                //focus tracking
-                var crossline = focus.append('g')
-                    .attr('id', 'crossline');
-                crossline.append('line')
-                    .attr('id', 'crossLineX')
-                    .attr('class', 'crossLine');
-                crossline.append('line')
-                    .attr('id', 'crossLineY')
-                    .attr('class', 'crossLine');
-                crossline.append('text')
-                    .attr('id', 'crossLineTip');
 
                 function mousemove() {
                     if (!currSeries || currSeries.length === 0) {
@@ -263,6 +302,7 @@ angular.module('argus.directives.charts.d3LineChartTest', [])
 
                 //Generate cross lines at the point/cursor
                 function generateCrossLine(mouseY, X, Y) {
+                    if(!mouseY) return;
                     focus.select('#crossLineX')
                         .attr('x1', X).attr('y1', 0)
                         .attr('x2', X).attr('y2', height);
@@ -286,7 +326,7 @@ angular.module('argus.directives.charts.d3LineChartTest', [])
                 //brushed
                 function brushed() {
                     // ignore the case when it is called by the zoomed function
-                    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return;
+                    if (d3.event.sourceEvent && (d3.event.sourceEvent.type === "zoom" || d3.event.sourceEvent.type === "resize")) return;
                     var s = d3.event.selection || x2.range();
                     x.domain(s.map(x2.invert, x2));     //rescale the domain of x axis
                                                         //invert the x value in brush axis range to the
@@ -372,8 +412,44 @@ angular.module('argus.directives.charts.d3LineChartTest', [])
                     y.domain(d3.extent(datapoints, function(d) {return d[1];}));
                 }
 
-                // Update graph on new metric results
-                scope.$watch(attrs.series, function(series) {
+                //resize
+                function resize(){
+                    var temp = x.domain(); //remember that when resize
+                    containerWidth = element.parent().width();
+
+                    width = containerWidth - marginLeft - marginRight;
+                    margin = {top: marginTop,
+                        right: marginRight,
+                        bottom: containerHeight - marginTop - height,
+                        left: marginLeft};
+
+                    margin2 = {top: containerHeight - height2 - marginBottom,
+                        right: marginRight,
+                        bottom: marginBottom,
+                        left: marginLeft};
+
+                    d3.select('svg').remove();
+                    setGraph(); //set up the chart
+                    updateGraph(currSeries); //refill the data draw the line
+
+                    //restore the zoom&brush
+                    x.domain(temp);
+
+                    //redraw the line&brush
+                    svg.selectAll(".line").attr("d", line);//redraw the line
+                    svg.selectAll(".x.axis").call(xAxis);  //redraw xAxis
+                    svg.selectAll(".x.grid").call(xGrid); //redraw the grid
+
+                    // restore the brush
+                    context.select(".brush").call
+                    (brush.move, [x2(temp[0]), x2(temp[1])]);
+
+                }
+                d3.select(window).on('resize', resize);
+
+
+                //updateGraph
+                function updateGraph(series){
                     if (!series) return;
 
                     var allDatapoints = [];
@@ -422,14 +498,19 @@ angular.module('argus.directives.charts.d3LineChartTest', [])
                         .call(yAxis);
                     svgTransition.select('.x.grid')
                         .duration(750)
-                        .call(xGrid)
+                        .call(xGrid);
                     svgTransition.select('.y.grid')
                         .duration(750)
-                        .call(yGrid)
+                        .call(yGrid);
 
                     svgTransition.select('.xBrush.axis')
                         .duration(750)
                         .call(xAxis2);
+                }
+
+                // Update graph on new metric results
+                scope.$watch(attrs.series, function(series) {
+                    updateGraph(series);
                 });
             }
         };
