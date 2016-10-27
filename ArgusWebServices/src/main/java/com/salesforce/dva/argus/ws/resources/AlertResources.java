5e0884ce31620c0e759851a11b6793d3178f0645
John Xu <liuxizi.xu@salesforce.com>
2016-10-27 11:10:39 -0700

    add a new method to return alertDtos without content

--- ArgusWebServices/src/main/java/com/salesforce/dva/argus/ws/resources/AlertResources.java
+++ ArgusWebServices/src/main/java/com/salesforce/dva/argus/ws/resources/AlertResources.java
@@ -28,7 +28,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-	 
+
 package com.salesforce.dva.argus.ws.resources;
 
 import com.salesforce.dva.argus.entity.Alert;
