42002c286ed985e60ae9da8e5969cf7b37ffc96a
Chris Nardi <cnardi@salesforce.com>
2017-01-10 13:14:21 -0800

    Controls includes ALL fields that the user can create in the 'markup' view of the Edit tab of a dashboard:support multiple attributes for control fields.

--- ArgusWeb/app/js/directives/controls/submit.js
+++ ArgusWeb/app/js/directives/controls/submit.js
@@ -1,15 +1,23 @@
 angular.module('argus.directives.controls.submit', [])
-.directive('agSubmit', ['$rootScope','$http', function($rootScope, $http) {
+.directive('agSubmit', ['$http', function($http) {
     return {
         restrict: 'E',
+        scope: {
+            controlName: '@name',
+            elemId: '@id',
+            cssName: '@class',
+            style: '@style',
+            size: '@size'
+        },
         require: '^agDashboard',
         template: '',
         link: function(scope, element, attributes, dashboardCtrl) {
-            var buttonName = 'Submit';
+            var buttonName = '';
             if (element.text() && element.text().length > 0) {
                 buttonName = element.text();
             }
-            element.html('<button class="btn btn-primary btn-md">' +  buttonName + '</button>');
+
+            element.html('<button id='+ scope.elemId +' class="btn btn-primary btn-md '+ scope.cssName +'" size='+ scope.size +' style='+ scope.style +'>' + buttonName + '</button>');
 
             element.on('click', function() {
                 $http.pendingRequests = []; //This line should be deleted.
