a87fbe87819df160c7fa00ba0d91ccc61b994b17
John Xu <liuxizi.xu@salesforce.com>
2017-02-06 22:33:35 -0800

    add hash number for js files

--- ArgusWeb/webpack.config.js
+++ ArgusWeb/webpack.config.js
@@ -1,5 +1,7 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ChunkManifestPlugin = require("chunk-manifest-webpack-plugin");
+var WebpackChunkHash = require("webpack-chunk-hash");
 
 var webpack = require('webpack');
 var path = require('path');
@@ -7,41 +9,16 @@ var path = require('path');
 module.exports = {
     context: __dirname + '/app',
     entry: {
-        app: './js/argus.js',
-        // the following are the bower stuff
-        // vendor: ['angular', 'angular-mocks', 'jquery', 'bootstrap', 'components-font-awesome', 'angular-route', 'angular-growl-v2', 'angular-animate', 'angular-resource', 'angular-utils-pagination', 'highstock-release', 'codemirror', 'angular-ui-codemirror', 'ngstorage', 'at-table', 'angulartics', 'angular-bootstrap', 'angular-bootstrap-datetimepicker', 'angular-selectize2', 'q', 'd3', 'd3-tip']
+        argus: './js/argus.js'
     },
     output: {
         path: __dirname + '/dist',
-        filename: 'argus.bundle.js'
+        filename: '[name].[chunkhash].js',
+        chunkFilename: "[name].[chunkhash].js"
     },
-    module: {
-        // rules: [
-        //     {
-        //         test: /\.css$/,
-        //         use: ['style-loader', 'css-loader']
-        //     },
-        //     {
-        //         test: /\.(jpe?g|png|gif)$/i,
-        //         loader: "file-loader?name=/img/[name].[ext]"
-        //     }
-        //     {
-        //         test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
-        //         loader: "file-loader"
-        //     }
-        rules: [
-            {
-                test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
-                loader: 'file-loader'
-            }
-        ]
-    },
-    // resolve: {
-    //     modules: [path.resolve(__dirname, "app"), "bower_components"],
-    //     descriptionFiles: ["package.json", "bower.json"],
-    // },
     plugins: [
         new CopyWebpackPlugin([
+            // TODO: need to make bower_components into vendor.js
             {from:'bower_components', to:'bower_components'},
             {from:'css', to:'css'},
             {from:'img', to:'img'},
@@ -51,11 +28,25 @@ module.exports = {
             template: __dirname + '/app/index.html',
             filename: 'index.html',
             inject: 'body'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: [/*"vendor", */"manifest"], // vendor libs + extracted manifest
+            minChunks: Infinity
+        }),
+        new webpack.HashedModuleIdsPlugin(),
+        new WebpackChunkHash(),
+        new ChunkManifestPlugin({
+            filename: "chunk-manifest.json",
+            manifestVariable: "webpackManifest"
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false
+            },
+            mangle: false
         })
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name: 'vendor',
-        //     filename: 'vendor.js'
-        // }),
-        // new webpack.optimize.UglifyJsPlugin()
     ]
 };
