76a4ab9e47c9ebedb9d7beb719558370a4265d00
Stefan Wiedemann <stefan.wiedemann@kiwigrid.com>
2017-03-06 11:41:55 +0100

    - interface change in discoveryservice - extend test

--- ArgusCore/src/test/java/com/salesforce/dva/argus/service/schema/MetricQueryMaskerTest.java
+++ ArgusCore/src/test/java/com/salesforce/dva/argus/service/schema/MetricQueryMaskerTest.java
@@ -29,7 +29,7 @@ public class MetricQueryMaskerTest {
 		String encodedExpression = URLEncoder.encode(filterExpression, "UTF-8");
 
 		Map<String, String> tagMap = new LinkedHashMap<>();
-		tagMap.put("readers", "regexp((^)test($|anything))");
+		tagMap.put("tag", filterExpression);
 
 		MetricQuery metricQuery = new MetricQuery("TestScope", "TestMetric", tagMap, 0l, 1l);
 
@@ -42,6 +42,32 @@ public class MetricQueryMaskerTest {
 		MetricQuery unmaskedQuery = metricQueryMasker.demaskQuery(maskedQuery);
 
 		assertTrue("The filter expression should be unmasked.", unmaskedQuery.toString().contains(encodedExpression));
+
+	}
+
+	@Test
+	public void testMaskAndDemaskQuery_multipleTags() throws Exception{
+
+		String filterExpression =   "literal_or(i)";
+		String unfilterdExpression = "*";
+		String encodedExpression = URLEncoder.encode(filterExpression, "UTF-8");
+
+		Map<String, String> tagMap = new LinkedHashMap<>();
+		tagMap.put("tag",filterExpression);
+		tagMap.put("tag2", unfilterdExpression);
+
+		MetricQuery metricQuery = new MetricQuery("TestScope", "TestMetric", tagMap, 0l, 1l);
+
+		MetricQuery maskedQuery = metricQueryMasker.maskQuery(metricQuery);
+
+		assertTrue("The query should be masked.", metricQueryMasker.isQueryMasked());
+		assertFalse("The filter expression should be masked.", maskedQuery.toString().contains(filterExpression));
+
+
+		MetricQuery unmaskedQuery = metricQueryMasker.demaskQuery(maskedQuery);
+
+		assertTrue("The filter expression should be unmasked.", unmaskedQuery.toString().contains(encodedExpression));
+		assertTrue("The filter expression should be unmasked.", unmaskedQuery.toString().contains(unfilterdExpression));
 
 	}
 
