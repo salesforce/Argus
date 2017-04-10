56c6d7654849a0a35e46824aa053af6556b6eb6b
Chris Nardi <cnardi@salesforce.com>
2017-04-10 16:58:10 -0700

    more eslint updates.

--- ArgusWeb/app/js/services/utilService.js
+++ ArgusWeb/app/js/services/utilService.js
@@ -1,61 +1,63 @@
+/*global angular:false, copyProperties:false */
+
 angular.module('argus.services.utils', [])
 .service('UtilService', [function() {
-    'use strict';
+	'use strict';
 
-    var options = {
-        assignController: function(controllers) {
-            if (!controllers) return;
-            for (var i=0; i < controllers.length; i++) {
-                if (controllers[i])
-                    return controllers[i];
-            }
-        },
+	var options = {
+		assignController: function(controllers) {
+			if (!controllers) return;
+			for (var i=0; i < controllers.length; i++) {
+				if (controllers[i])
+					return controllers[i];
+			}
+		},
 
-        copyProperties: function(from, to) {
-            for (var key in from) {
-                if (from.hasOwnProperty(key)) {
-                    //if from[key] is not an object and is last property then just copy so that it will overwrite the existing value
-                    if (!to[key] || typeof from[key] == 'string' || from[key] instanceof String ) {
-                        to[key] = from[key];
-                    } else {
-                        copyProperties(from[key],to[key]);
-                    }
-                }
-            }
-        },
+		copyProperties: function(from, to) {
+			for (var key in from) {
+				if (from.hasOwnProperty(key)) {
+					//if from[key] is not an object and is last property then just copy so that it will overwrite the existing value
+					if (!to[key] || typeof from[key] == 'string' || from[key] instanceof String ) {
+						to[key] = from[key];
+					} else {
+						copyProperties(from[key],to[key]);
+					}
+				}
+			}
+		},
 
-        constructObjectTree: function(name, value) {
-            var result = {};
-            var index = name.indexOf('.');
+		constructObjectTree: function(name, value) {
+			var result = {};
+			var index = name.indexOf('.');
 
-            if (index == -1) {
-                result[name] = this.getParsedValue(value);
-                return result;
-            } else {
-                var property = name.substring(0, index);
-                result[property] = this.constructObjectTree(name.substring(index + 1), value);
-                return result;
-            }
-        },
+			if (index == -1) {
+				result[name] = this.getParsedValue(value);
+				return result;
+			} else {
+				var property = name.substring(0, index);
+				result[property] = this.constructObjectTree(name.substring(index + 1), value);
+				return result;
+			}
+		},
 
-        getParsedValue: function(value) {
-            if (value instanceof Object || value.length == 0) {
-                return value;
-            }
-            if (value == 'true') {
-                return true;
-            } else if (value == 'false') {
-                return false;
-            } else if (!isNaN(value)) {
-                return parseInt(value);
-            }
-            return value;
-        },
+		getParsedValue: function(value) {
+			if (value instanceof Object || value.length == 0) {
+				return value;
+			}
+			if (value == 'true') {
+				return true;
+			} else if (value == 'false') {
+				return false;
+			} else if (!isNaN(value)) {
+				return parseInt(value);
+			}
+			return value;
+		},
 
-        cssNotationCharactersConverter: function (name) {
-            return name.replace( /(:|\.|\[|\]|,|=|@)/g, "\\$1" );
-        }
-    };
+		cssNotationCharactersConverter: function (name) {
+			return name.replace( /(:|\.|\[|\]|,|=|@)/g, '\\$1' );
+		}
+	};
 
-    return options;
+	return options;
 }]);
