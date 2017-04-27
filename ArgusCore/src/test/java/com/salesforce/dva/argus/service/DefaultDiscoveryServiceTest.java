74de24c2199bae2342b89e9586f1011d20cdb7f7
Bhinav Sura <bhinav.sura@salesforce.com>
2017-04-26 17:10:08 -0700

    DefaultDiscoveryService ConcurrentModificationException bugfix.

--- ArgusCore/src/test/java/com/salesforce/dva/argus/service/DefaultDiscoveryServiceTest.java
+++ ArgusCore/src/test/java/com/salesforce/dva/argus/service/DefaultDiscoveryServiceTest.java
@@ -32,13 +32,17 @@
 package com.salesforce.dva.argus.service;
 
 import com.salesforce.dva.argus.AbstractTest;
+import com.salesforce.dva.argus.entity.Metric;
 import com.salesforce.dva.argus.entity.MetricSchemaRecord;
 import com.salesforce.dva.argus.entity.MetricSchemaRecordQuery;
+import com.salesforce.dva.argus.service.SchemaService.RecordType;
 import com.salesforce.dva.argus.service.schema.DefaultDiscoveryService;
+import com.salesforce.dva.argus.service.schema.ElasticSearchSchemaService;
 import com.salesforce.dva.argus.service.schema.WildcardExpansionLimitExceededException;
 import com.salesforce.dva.argus.service.tsdb.MetricQuery;
 import com.salesforce.dva.argus.service.tsdb.MetricQuery.Aggregator;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -52,6 +56,77 @@ import static org.mockito.Mockito.*;
 
 
 public class DefaultDiscoveryServiceTest extends AbstractTest {
+	
+	@Test
+	@Ignore
+	public void testElasticSearch_Put() {
+		ElasticSearchSchemaService service = new ElasticSearchSchemaService(system.getConfiguration());
+		
+		List<Metric> metrics = createRandomMetrics(null, null, 10);
+		service.put(metrics);
+	}
+	
+	@Test
+	@Ignore
+	public void testElasticSearch_Search() {
+		ElasticSearchSchemaService service = new ElasticSearchSchemaService(system.getConfiguration());
+		
+		List<MetricSchemaRecord> records = service.keywordSearch("redis", 500, 1);
+		for(MetricSchemaRecord record : records) {
+			System.out.println(MetricSchemaRecord.print(record));
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void testElasticSearch_Get() {
+		ElasticSearchSchemaService service = new ElasticSearchSchemaService(system.getConfiguration());
+		
+		MetricSchemaRecordQuery query = new MetricSchemaRecordQuery("*", "*f0675a53ea898665", "AbstractTest-15b64863b7d-9c5*", "*", "*");
+		List<MetricSchemaRecord> records = service.get(query, 10, 1);
+		for(MetricSchemaRecord record : records) {
+			System.out.println(MetricSchemaRecord.print(record));
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void testElasticSearch_GetUnique() {
+		ElasticSearchSchemaService service = new ElasticSearchSchemaService(system.getConfiguration());
+		
+		MetricSchemaRecordQuery query = new MetricSchemaRecordQuery("*", "*f0675a53ea898665", "AbstractTest-15b64863b7d-9c5*", "*", "*");
+		List<String> records = service.getUnique(query, 10, 1, RecordType.SCOPE);
+		for(String record : records) {
+			System.out.println(record);
+		}
+	}
+	
+	protected List<Metric> createRandomMetrics(String scope, String metric, int count) {
+        List<Metric> result = new ArrayList<>(count);
+
+        scope = scope == null ? createRandomName() : scope;
+
+        String tag = createRandomName();
+
+        for (int i = 0; i < count; i++) {
+            String metricName = metric == null ? createRandomName() : metric;
+            Metric met = new Metric(scope, metricName);
+            int datapointCount = 1;
+            Map<Long, Double> datapoints = new HashMap<>();
+            long start = System.currentTimeMillis() - 60000L;
+
+            for (int j = 0; j < datapointCount; j++) {
+                datapoints.put(start - (j * 60000L), (double)(random.nextInt(100) + 1));
+            }
+            met.setDatapoints(datapoints);
+            met.setDisplayName(createRandomName());
+            met.setUnits(createRandomName());
+            met.setTag(tag, String.valueOf(i));
+            result.add(met);
+        }
+        
+        return result;
+    }
 
     @Test
     public void testWildcardQueriesMatchWithinLimit() {
@@ -99,6 +174,37 @@ public class DefaultDiscoveryServiceTest extends AbstractTest {
         tags.put("device", "device[1]");
 
         MetricQuery query = new MetricQuery("scope0", "metric0", tags, 1L, 2L);
+        List<MetricQuery> matchedQueries = discoveryService.getMatchingQueries(query);
+        
+        assertTrue(matchedQueries.isEmpty());
+    }
+    
+    /**
+	 * Assume that following schemarecords exist in the database:
+	 * scope0,metric0,source,unittest0,null
+	 * scope0,metric0,source,unittest1,null
+	 * scope0,metric0,device,device0,null
+	 * scope1,metric0,source,unittest0,null
+	 * scope1,metric0,source,unittest1,null
+	 * scope1,metric0,device,device0,null
+	 */
+    @Test
+    public void testWildcardQueriesMatchMultipleTags1() {
+    	
+    	SchemaService schemaServiceMock = mock(SchemaService.class);
+        
+        MetricSchemaRecordQuery queryForTag1 = new MetricSchemaRecordQuery(null, "scope?", "metric0", "source", "unittest0");
+        MetricSchemaRecordQuery queryForTag2 = new MetricSchemaRecordQuery(null, "scope?", "metric0", "device", "device[1]");
+        when(schemaServiceMock.get(queryForTag1, 500, 1)).thenReturn(Arrays.asList(new MetricSchemaRecord(null, "scope0", "metric0", "source", "unittest0"), new MetricSchemaRecord(null, "scope1", "metric0", "source", "unittest0")));
+        when(schemaServiceMock.get(queryForTag2, 500, 1)).thenReturn(new ArrayList<>());
+        
+        DefaultDiscoveryService discoveryService = new DefaultDiscoveryService(schemaServiceMock, system.getConfiguration());
+
+        Map<String, String> tags = new HashMap<String, String>();
+        tags.put("source", "unittest0");
+        tags.put("device", "device[1]");
+
+        MetricQuery query = new MetricQuery("scope?", "metric0", tags, 1L, 2L);
         List<MetricQuery> matchedQueries = discoveryService.getMatchingQueries(query);
         
         assertTrue(matchedQueries.isEmpty());
