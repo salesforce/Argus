184ed006fb73d8ee66f3d5f5966283c5ea1f98e2
John Xu <liuxizi.xu@salesforce.com>
2017-04-27 14:29:59 -0700

    merge from upstream and add scatter plot graph

--- ArgusWeb/app/js/directives/charts/metric.js
+++ ArgusWeb/app/js/directives/charts/metric.js
@@ -1,44 +1,47 @@
+'use strict';
+/*global angular:false */
+
 angular.module('argus.directives.charts.metric', [])
 .directive('agMetric', ['UtilService', function(UtilService) {
-    var metricNameIndex = 100;
-    return {
-        restrict: 'E',
-        require: ['?^agChart', '?^agStatusIndicator', '?^agHeatmap', '?^agTable'],
-        scope: {
-            expression: '@'
-        },
-        controller: 'metricElements',
-        template: '',
-        link: function(scope, element, attributes, controllers) {
-            var elementCtrl;
-            var value = '';
-            var seriesData = {};
-            var metricName = 'metric_' + metricNameIndex++;
+	var metricNameIndex = 100;
+	return {
+		restrict: 'E',
+		require: ['?^agChart', '?^agStatusIndicator', '?^agHeatmap', '?^agTable'],
+		scope: {
+			expression: '@'
+		},
+		controller: 'metricElements',
+		template: '',
+		link: function(scope, element, attributes, controllers) {
+			var elementCtrl;
+			var value = '';
+			var seriesData = {};
+			var metricName = 'metric_' + metricNameIndex++;
 
-            // assign proper controller
-            elementCtrl = UtilService.assignController(controllers);
+			// assign proper controller
+			elementCtrl = UtilService.assignController(controllers);
 
-            // separate specific series data from other attributes
-            // 'color' & 'name' are used to supplement the 'series' data when rendering a chart
-            seriesData.color = attributes.seriescolor;
-            seriesData.name = attributes.seriesname;
+			// separate specific series data from other attributes
+			// 'color' & 'name' are used to supplement the 'series' data when rendering a chart
+			seriesData.color = attributes.seriescolor;
+			seriesData.name = attributes.seriesname;
 
-            if (attributes.value && attributes.value.length > 0) {
-                value = attributes.value;
-            } else {
-                value = element.text();
-            }
+			if (attributes.value && attributes.value.length > 0) {
+				value = attributes.value;
+			} else {
+				value = element.text();
+			}
 
-            if (value && value.length > 0) {
-                elementCtrl.updateMetric(metricName, value.replace(/(\r\n|\n|\r|\s+)/gm,""), scope.metricOptions, seriesData);
-            }
+			if (value && value.length > 0) {
+				elementCtrl.updateMetric(metricName, value.replace(/(\r\n|\n|\r|\s+)/gm, ''), scope.metricOptions, seriesData);
+			}
 
-            scope.$watch('expression', function(newValue, oldValue) {
-                if (newValue) {
-                    elementCtrl.updateMetric(metricName, newValue, scope.metricOptions, seriesData);
-                }
-            });
-            element.html('<span> </span>');
-        }
-    }
+			scope.$watch('expression', function(newValue) {
+				if (newValue) {
+					elementCtrl.updateMetric(metricName, newValue, scope.metricOptions, seriesData);
+				}
+			});
+			element.html('<span> </span>');
+		}
+	};
 }]);
