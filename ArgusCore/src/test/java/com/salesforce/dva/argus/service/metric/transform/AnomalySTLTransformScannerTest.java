904f76b4eb0c6f2ef32436ab5d6a9c26d3c4b178
adechambers <adelaide.chambers@salesforce.com>
2017-07-12 12:25:52 -0700

    Update AnomalySTLTransformScannerTest.java

--- ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/AnomalySTLTransformScannerTest.java
+++ ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/AnomalySTLTransformScannerTest.java
@@ -457,4 +457,75 @@ public class AnomalySTLTransformScannerTest extends AbstractTest {
 			assert(expected.equals(actual));
 		}
 	}
+	
+	@Test
+	public void testDPIntegrityAndDisposal() {
+		
+		MetricScanner.setChunkPercentage(0.50);
+		
+		TSDBService serviceMock = mock(TSDBService.class);
+		List<Metric> metrics = createRandomMetrics(null, null, 10);
+		for (Metric m : metrics) {
+			Map<Long, Double> datapoints = new HashMap<>(m.getDatapoints());
+			if (m.getDatapoints().size() < 4) {
+				Long baseTime = datapoints.keySet().iterator().next();
+				datapoints.put(baseTime + 1000L, random.nextDouble() * 500);
+				datapoints.put(baseTime + 2000L, random.nextDouble() * 500);
+				datapoints.put(baseTime + 3000L, random.nextDouble() * 500);
+			}
+			if (datapoints.size() % 2 == 0) {
+				datapoints.put(Collections.max(datapoints.keySet()) + 1000L, random.nextDouble() * 500); // guaranteed to be a unique point
+			}
+			m.setDatapoints(datapoints);
+			assert(m.getDatapoints().size() >= 4);
+		}
+		List<MetricQuery> queries = toQueries(metrics);
+		List<MetricScanner> scanners = new ArrayList<>();
+		
+		for (int i = 0; i < metrics.size(); i++) {
+			Metric m = metrics.get(i);
+			MetricQuery q = queries.get(i);
+						
+			Long bound = q.getStartTimestamp() + (q.getEndTimestamp() - q.getStartTimestamp()) / 2;
+			List<MetricQuery> highQuery = new ArrayList<>();
+			highQuery.add(new MetricQuery(q.getScope(), q.getMetric(), q.getTags(), bound, q.getEndTimestamp()));
+			List<MetricQuery> tooHigh = new ArrayList<>();
+			tooHigh.add(new MetricQuery(q.getScope(), q.getMetric(), q.getTags(), q.getEndTimestamp(), q.getEndTimestamp()));
+			
+			MetricScanner s = new MetricScanner(lowElems(m, bound), q, serviceMock, bound);
+			scanners.add(s);
+			
+			when(serviceMock.getMetrics(tooHigh)).thenReturn(outOfBounds());
+			when(serviceMock.getMetrics(highQuery)).thenReturn(filterOver(m, bound, highQuery.get(0)));
+		}
+
+		
+		AnomalySTLTransform transform = new AnomalySTLTransform();
+		
+		List<String> constants = new ArrayList<>(1);
+		constants.add(null);
+		
+		for (int i = 0; i < metrics.size(); i++) {
+			List<Metric> mList = new ArrayList<>();
+			mList.add(metrics.get(i));
+			List<MetricScanner> msList = new ArrayList<>();
+			msList.add(scanners.get(i));
+			
+			int lower = metrics.get(i).getDatapoints().size() / 2 == 2 ? 0 : random.nextInt(metrics.get(i).getDatapoints().size() / 2 - 2);
+			constants.set(0, "" + (lower + 2));
+			
+			List<Metric> expected = transform.transform(mList, constants);
+			List<Metric> actual = transform.transformScanner(msList, constants);
+			
+			assert(expected.equals(actual));
+			
+			double epsilon = Math.pow(10, -6);
+			for (int j = 0; j < expected.size(); j++) {
+				for (Long time : expected.get(j).getDatapoints().keySet()) {
+					assert(Math.abs(expected.get(j).getDatapoints().get(time) - actual.get(j).getDatapoints().get(time)) < epsilon);
+				}
+			}
+			assert(!MetricScanner.existingScanner(metrics.get(i), queries.get(i)));
+		}
+	}
 }
