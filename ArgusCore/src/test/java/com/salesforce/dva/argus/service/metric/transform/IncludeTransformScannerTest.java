2532553331db43822684d6ad340884a4b3785874
adechambers <adelaide.chambers@salesforce.com>
2017-07-12 11:00:15 -0700

    Update IncludeTransformScannerTest.java

--- ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/IncludeTransformScannerTest.java
+++ ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/IncludeTransformScannerTest.java
@@ -193,4 +193,52 @@ public class IncludeTransformScannerTest extends AbstractTest {
 			assert(expected.get(i).getDatapoints().equals(actual.get(i).getDatapoints()));
 		}
 	}
+	
+	@Test
+	public void testDisposeOfUnusedScanners() {
+		
+		MetricScanner.setChunkPercentage(0.50);
+		
+		TSDBService serviceMock = mock(TSDBService.class);
+		List<Metric> metrics = createRandomMetrics(null, null, 10);
+		for (int i = 0; i < metrics.size(); i++) {
+			metrics.get(i).setMetric(TEST_METRIC + "-" + i); 
+		}
+		List<MetricQuery> queries = toQueries(metrics);
+		List<MetricScanner> scanners = new ArrayList<>();
+		
+		for (int i = 0; i < metrics.size(); i++) {
+			Metric m = metrics.get(i);
+			MetricQuery q = queries.get(i);
+						
+			Long bound = q.getStartTimestamp() + (q.getEndTimestamp() - q.getStartTimestamp()) / 2;
+			List<MetricQuery> highQuery = new ArrayList<>();
+			highQuery.add(new MetricQuery(q.getScope(), q.getMetric(), q.getTags(), bound, q.getEndTimestamp()));
+			List<MetricQuery> tooHigh = new ArrayList<>();
+			tooHigh.add(new MetricQuery(q.getScope(), q.getMetric(), q.getTags(), q.getEndTimestamp(), q.getEndTimestamp()));
+			
+			MetricScanner s = new MetricScanner(lowElems(m, bound), q, serviceMock, bound);
+			scanners.add(s);
+			
+			when(serviceMock.getMetrics(tooHigh)).thenReturn(outOfBounds());
+			when(serviceMock.getMetrics(highQuery)).thenReturn(filterOver(m, bound, highQuery.get(0)));
+		}
+		
+		IncludeTransform transform = new IncludeTransform();
+		List<String> constants = new ArrayList<>();
+		constants.add(TEST_INCLUDE_REGEX);
+		
+		List<Metric> expected = transform.transform(metrics, constants);
+		List<Metric> actual = transform.transformScanner(scanners, constants);
+		
+		assert(expected.equals(actual));
+				
+		for (int i = 0; i < expected.size(); i++) {
+			assert(expected.get(i).getDatapoints().equals(actual.get(i).getDatapoints()));
+		}
+		
+		for (int i = 0; i < metrics.size(); i++) {
+			assert(!MetricScanner.existingScanner(metrics.get(i), queries.get(i)));
+		}
+	}
 }
