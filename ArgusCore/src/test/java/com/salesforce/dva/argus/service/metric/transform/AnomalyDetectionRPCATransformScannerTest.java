bfed8ca700aaac3a33ee5bad2542f894768ff00b
adechambers <adelaide.chambers@salesforce.com>
2017-07-12 12:14:36 -0700

    Update AnomalyDetectionRPCATransformScannerTest.java

--- ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/AnomalyDetectionRPCATransformScannerTest.java
+++ ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/AnomalyDetectionRPCATransformScannerTest.java
@@ -248,4 +248,58 @@ public class AnomalyDetectionRPCATransformScannerTest extends AbstractTest {
 		}
 		
 	}
+	
+	@Test
+	public void testDPIntegrityAndDisposal() {
+		
+		MetricScanner.setChunkPercentage(0.50);
+		
+		TSDBService serviceMock = mock(TSDBService.class);
+		List<Metric> metrics = createRandomMetrics(null, null, 10);
+		List<MetricQuery> queries = toQueries(metrics);
+		List<MetricScanner> scanners = new ArrayList<>();
+		
+		for (int i = 0; i < metrics.size(); i++) {
+			Metric m = metrics.get(i);
+			MetricQuery q = queries.get(i);
+						
+			Long bound = q.getStartTimestamp() + (q.getEndTimestamp() - q.getStartTimestamp()) / 2;
+			List<MetricQuery> highQuery = new ArrayList<>();
+			highQuery.add(new MetricQuery(q.getScope(), q.getMetric(), q.getTags(), bound, q.getEndTimestamp()));
+			List<MetricQuery> tooHigh = new ArrayList<>();
+			tooHigh.add(new MetricQuery(q.getScope(), q.getMetric(), q.getTags(), q.getEndTimestamp(), q.getEndTimestamp()));
+			
+			MetricScanner s = new MetricScanner(lowElems(m, bound), q, serviceMock, bound);
+			scanners.add(s);
+			
+			when(serviceMock.getMetrics(tooHigh)).thenReturn(outOfBounds());
+			when(serviceMock.getMetrics(highQuery)).thenReturn(filterOver(m, bound, highQuery.get(0)));
+		}
+		
+		AnomalyDetectionGaussianDensityTransform transform = new AnomalyDetectionGaussianDensityTransform();
+		List<String> constants = new ArrayList<>();
+		constants.add("2h");
+		
+		for (int i = 0; i < metrics.size(); i++) {
+			List<Metric> mList = new ArrayList<>();
+			mList.add(metrics.get(i));
+			List<MetricScanner> msList = new ArrayList<>();
+			msList.add(scanners.get(i));
+						
+			List<Metric> expected = transform.transform(mList, constants);
+			List<Metric> actual = transform.transformScanner(msList, constants);
+			
+			assert(expected.equals(actual));
+			
+			double epsilon = Math.pow(10, -6);
+			
+			for (int j = 0; j < expected.size(); j++) {
+				for (Long time : expected.get(j).getDatapoints().keySet()) {
+					assert(Math.abs(expected.get(j).getDatapoints().get(time) - actual.get(j).getDatapoints().get(time)) < epsilon);
+				}
+			}
+			
+			assert(!MetricScanner.existingScanner(metrics.get(i), queries.get(i)));
+		}
+	}
 }
