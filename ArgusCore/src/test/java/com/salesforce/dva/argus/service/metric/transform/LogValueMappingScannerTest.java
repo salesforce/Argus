8024ad6cc2969dc11ad0e0bd750145c4bfcc62ba
adechambers <adelaide.chambers@salesforce.com>
2017-07-12 15:07:37 -0700

    Update LogValueMappingScannerTest.java

--- ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/LogValueMappingScannerTest.java
+++ ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/LogValueMappingScannerTest.java
@@ -173,4 +173,44 @@ public class LogValueMappingScannerTest extends AbstractTest {
 			assert(expected.equals(actual));
 		}
 	}
+	
+	@Test
+	public void testDPIntegrityAndDisposal() {
+		MetricScanner.setChunkPercentage(0.50);
+		
+		TSDBService serviceMock = mock(TSDBService.class);
+		List<Metric> metrics = createRandomMetrics(null, null, 10);
+		List<MetricQuery> queries = toQueries(metrics);
+		List<MetricScanner> scanners = new ArrayList<>();
+		
+		for (int i = 0; i < metrics.size(); i++) {
+			Metric m = metrics.get(i);
+			MetricQuery q = queries.get(i);
+						
+			Long bound = q.getStartTimestamp() + (q.getEndTimestamp() - q.getStartTimestamp()) / 2;
+			List<MetricQuery> highQuery = new ArrayList<>();
+			highQuery.add(new MetricQuery(q.getScope(), q.getMetric(), q.getTags(), bound, q.getEndTimestamp()));
+			List<MetricQuery> tooHigh = new ArrayList<>();
+			tooHigh.add(new MetricQuery(q.getScope(), q.getMetric(), q.getTags(), q.getEndTimestamp(), q.getEndTimestamp()));
+			
+			MetricScanner s = new MetricScanner(lowElems(m, bound), q, serviceMock, bound);
+			scanners.add(s);
+			
+			when(serviceMock.getMetrics(tooHigh)).thenReturn(outOfBounds());
+			when(serviceMock.getMetrics(highQuery)).thenReturn(filterOver(m, bound, highQuery.get(0)));
+		}
+		
+		LogValueMapping map = new LogValueMapping();
+		List<String> constants = new ArrayList<>(1);
+		constants.add("" + random.nextDouble() * 10);
+		
+		for (int i = 0; i < metrics.size(); i++) {
+			Map<Long, Double> dps = new HashMap<>(metrics.get(i).getDatapoints());
+			Map<Long, Double> expected = map.mapping(dps, constants);
+			Map<Long, Double> actual = map.mappingScanner(scanners.get(i), constants);
+			
+			assert(expected.equals(actual));
+			assert(!MetricScanner.existingScanner(metrics.get(i), queries.get(i)));
+		}
+	}
 }
