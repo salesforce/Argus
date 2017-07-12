68100c7e270719a0f47d23809a71dd9eabe48cf0
adechambers <adelaide.chambers@salesforce.com>
2017-07-12 15:00:45 -0700

    Update IntegralValueMappingScannerTest.java

--- ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/IntegralValueMappingScannerTest.java
+++ ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/IntegralValueMappingScannerTest.java
@@ -133,4 +133,41 @@ public class IntegralValueMappingScannerTest extends AbstractTest {
 		}
 	}
 	
+	@Test
+	public void testDPIntegrityAndDisposal() {
+		MetricScanner.setChunkPercentage(0.50);
+		
+		TSDBService serviceMock = mock(TSDBService.class);
+		List<Metric> metrics = createRandomMetrics(null, null, 10);
+		List<MetricQuery> queries = toQueries(metrics);
+		List<MetricScanner> scanners = new ArrayList<>();
+		
+		for (int i = 0; i < metrics.size(); i++) {
+			Metric m = metrics.get(i);
+			MetricQuery q = queries.get(i);
+						
+			Long bound = q.getStartTimestamp() + (q.getEndTimestamp() - q.getStartTimestamp()) / 2;
+			List<MetricQuery> highQuery = new ArrayList<>();
+			highQuery.add(new MetricQuery(q.getScope(), q.getMetric(), q.getTags(), bound, q.getEndTimestamp()));
+			List<MetricQuery> tooHigh = new ArrayList<>();
+			tooHigh.add(new MetricQuery(q.getScope(), q.getMetric(), q.getTags(), q.getEndTimestamp(), q.getEndTimestamp()));
+			
+			MetricScanner s = new MetricScanner(lowElems(m, bound), q, serviceMock, bound);
+			scanners.add(s);
+			
+			when(serviceMock.getMetrics(tooHigh)).thenReturn(outOfBounds());
+			when(serviceMock.getMetrics(highQuery)).thenReturn(filterOver(m, bound, highQuery.get(0)));
+		}
+		
+		IntegralValueMapping map = new IntegralValueMapping();
+		
+		for (int i = 0; i < metrics.size(); i++) {
+			Map<Long, Double> expected = map.mapping(metrics.get(i).getDatapoints());
+			Map<Long, Double> actual = map.mappingScanner(scanners.get(i));
+			
+			assert(expected.equals(actual));
+			assert(!MetricScanner.existingScanner(metrics.get(i), queries.get(i)));
+		}
+	}
+	
 }
