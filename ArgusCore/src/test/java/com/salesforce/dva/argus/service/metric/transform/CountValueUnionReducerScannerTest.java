6249bc13e80f517a025fd957e9352a3a8191afe5
adechambers <adelaide.chambers@salesforce.com>
2017-07-18 12:00:10 -0700

    Update CountValueUnionReducerScannerTest.java

--- ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/CountValueUnionReducerScannerTest.java
+++ ArgusCore/src/test/java/com/salesforce/dva/argus/service/metric/transform/CountValueUnionReducerScannerTest.java
@@ -4,7 +4,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-//import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
