26d9d137c7763dec50e4707b53063994a2c88c04
Justin Harringa <jharringa@salesforce.com>
2017-07-25 17:14:53 -0700

    More prod-ready .travis.deploy.sh

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cp .travis.settings.xml $HOME/.m2/settings.xml
-ls $HOME/.m2/settings.xml
 # Builds top level pom w/ zip
-#mvn --non-recursive -DskipTests -DskipDockerBuild -U -Prelease -Dproject.version=${PROJECT_VERSION} clean install
-# mvn -DskipDockerBuild -DskipTests -Dproject.version=${PROJECT_VERSION} -Prelease clean deploy
+# TODO: Figure out if ${PROJECT_VERSION} should be manually incremented in the POM or handled some other way
+mvn --batch-mode --non-recursive -DskipTests -DskipDockerBuild -U -Prelease -Dproject.version=${PROJECT_VERSION} clean install
+mvn --batch-mode -DskipDockerBuild -DskipTests -Dproject.version=${PROJECT_VERSION} -Prelease clean deploy
