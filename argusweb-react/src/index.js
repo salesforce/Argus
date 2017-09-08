f6b703e75e483e713dcc58ab5bcfa9b121dd328a
prestonfff <pfu@salesforce.com>
2017-09-08 14:48:07 -0700

    Add eslint and import style for Login page

--- argusweb-react/src/index.js
+++ argusweb-react/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Login from './Login';
 import registerServiceWorker from './registerServiceWorker';
 
