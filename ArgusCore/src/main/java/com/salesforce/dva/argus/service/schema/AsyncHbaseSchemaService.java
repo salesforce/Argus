e117e434e6e06702104302f3e618a077ed819707
Bhinav Sura <bhinav.sura@salesforce.com>
2017-09-18 18:49:58 -0700

    Change from TreeSet to HashSet.

--- ArgusCore/src/main/java/com/salesforce/dva/argus/service/schema/AsyncHbaseSchemaService.java
+++ ArgusCore/src/main/java/com/salesforce/dva/argus/service/schema/AsyncHbaseSchemaService.java
@@ -66,6 +66,7 @@ import java.nio.charset.Charset;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
 import java.util.Map.Entry;
@@ -345,7 +346,7 @@ public class AsyncHbaseSchemaService extends AbstractSchemaService {
     	
     	_logger.info("Using FastScan. Will skip rows while scanning.");
     	
-    	final Set<MetricSchemaRecord> records = new TreeSet<>();
+    	final Set<MetricSchemaRecord> records = new HashSet<>();
     	
     	final ScanMetadata metadata = _constructScanMetadata(query);
         String namespace = SchemaService.convertToRegex(query.getNamespace());
