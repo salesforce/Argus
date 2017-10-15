cd1ef57f861edba356c597e71232c6c2f0ddc7bd
Justin Harringa <justin@harringa.com>
2017-10-15 00:12:57 -0700

    Use project.version file to increment/release ver#

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cp .travis.settings.xml $HOME/.m2/settings.xml
+# PROJECT_VERSION in the commands below should be incremented and sourced from project.version
+source project.version
 # Builds top level pom w/ zip
-# TODO: Figure out if ${PROJECT_VERSION} should be manually incremented in the POM or handled some other way
 mvn --batch-mode --non-recursive -DskipTests -DskipDockerBuild -U -Prelease -Dproject.version=${PROJECT_VERSION} clean install
 mvn --batch-mode -DskipDockerBuild -DskipTests -Dproject.version=${PROJECT_VERSION} -Prelease clean deploy
