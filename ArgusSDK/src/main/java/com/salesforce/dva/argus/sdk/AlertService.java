bb2eaf65f3f4739a526d7499f1ae9528ad9bbe7e
Colby Guan <colbyguan@gmail.com>
2017-10-19 16:11:19 -0700

    change 'excpetions' package to 'exceptions'

--- ArgusSDK/src/main/java/com/salesforce/dva/argus/sdk/AlertService.java
+++ ArgusSDK/src/main/java/com/salesforce/dva/argus/sdk/AlertService.java
@@ -36,7 +36,7 @@ import com.salesforce.dva.argus.sdk.ArgusService.EndpointService;
 import com.salesforce.dva.argus.sdk.entity.Alert;
 import com.salesforce.dva.argus.sdk.entity.Notification;
 import com.salesforce.dva.argus.sdk.entity.Trigger;
-import com.salesforce.dva.argus.sdk.excpetions.TokenExpiredException;
+import com.salesforce.dva.argus.sdk.exceptions.TokenExpiredException;
 
 import java.io.IOException;
 import java.math.BigInteger;
