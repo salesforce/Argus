817cf7ee4ac2a59e0a79c6288bb7795a48fe23a4
Dilip <ddevaraj@salesforce.com>
2018-02-07 21:31:40 -0800

    avg implementation by zimsum and count

--- ArgusCore/src/main/java/com/salesforce/dva/argus/service/tsdb/EndPointQueryFederation.java
+++ ArgusCore/src/main/java/com/salesforce/dva/argus/service/tsdb/EndPointQueryFederation.java
@@ -26,20 +26,7 @@ public class EndPointQueryFederation extends QueryFederation{
 		for (MetricQuery query : queries) {
 			List<MetricQuery> subQueries = new ArrayList<>();
 			for (String readEndPoint : _readEndPoints) {
-				
 				MetricQuery mq = new MetricQuery(query);
-				
-				if(mq.getAggregator().equals(Aggregator.AVG)){
-					MetricQuery mq2 = new MetricQuery(query);
-					mq2.setAggregator(Aggregator.COUNT);
-					MetricQuery.MetricQueryContext metricQueryContext = mq2.new MetricQueryContext();
-					metricQueryContext.setReadEndPoint(readEndPoint);
-					mq2.setMetricQueryContext(metricQueryContext);
-					subQueries.add(mq2);
-
-					mq.setAggregator(Aggregator.ZIMSUM);
-				}
-				
 				MetricQuery.MetricQueryContext metricQueryContext = mq.new MetricQueryContext();
 				metricQueryContext.setReadEndPoint(readEndPoint);
 				mq.setMetricQueryContext(metricQueryContext);
