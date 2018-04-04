e0a142f6537671f0b54d9f9c98eae2dcae5aa40f
Justin Harringa <justin@harringa.com>
2018-04-04 13:03:12 -0700

    Improve comments in .travis.deploy.sh

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -7,10 +7,14 @@ gpg --fast-import codesigning.asc
 # Remove code signing keys for cleanliness
 rm codesigning.asc
 
+# Utilize tokenized `.travis.settings.xml` to do deployment
 cp .travis.settings.xml $HOME/.m2/settings.xml
+
 # PROJECT_VERSION in the commands below should be incremented and sourced from project.version
 source project.version
-# Builds top level pom w/ zip
+
+# Set the version of all `pom.xml`s to ${PROJECT_VERSION} from the `project.version` file
 mvn versions:set -DnewVersion=${PROJECT_VERSION}
+# Builds top level pom w/ zip
 mvn --batch-mode --non-recursive -DskipTests -DskipDockerBuild -U -Prelease -Dproject.version=${PROJECT_VERSION} clean install
 mvn --batch-mode -DskipDockerBuild -DskipTests -Dproject.version=${PROJECT_VERSION} -Prelease clean deploy
