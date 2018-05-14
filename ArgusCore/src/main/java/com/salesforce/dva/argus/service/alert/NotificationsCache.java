1e7d8de990bcf84f19c74227b2ce1e0b8c9321ec
Sundeep Tiyyagura <stiyyagura@salesforce.com>
2018-05-14 15:37:11 -0700

    Made changes to fetch the latest notifications status and store them in a local cache on each of the alert clients. This cache will be periodically refreshed every minute. With this change, there will be very minimal amount of queries made to postgres database during alert evaluation

---
+++ ArgusCore/src/main/java/com/salesforce/dva/argus/service/alert/NotificationsCache.java
@@ -0,0 +1,48 @@
+package com.salesforce.dva.argus.service.alert;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+
+public class NotificationsCache {
+	
+	private NotificationsCacheRefresherThread refresherThread;
+
+	private Map<BigInteger/*notificationId*/, Map<String/*metricKey*/, Long/*coolDownExpiration*/>> notificationCooldownExpirationMap = new HashMap<BigInteger, Map<String, Long>>();
+
+	private Map<BigInteger/*notificationId*/, Map<String/*metricKey*/, Boolean/*activeStatus*/>> notificationActiveStatusMap = new HashMap<BigInteger, Map<String, Boolean>>();
+
+	private boolean isNotificationsCacheRefreshed = false;
+	
+	public NotificationsCache(EntityManager em) {
+		refresherThread = new NotificationsCacheRefresherThread(this, em);
+		refresherThread.setDaemon(true);
+		refresherThread.start();
+	}
+
+	public Map<BigInteger, Map<String, Long>> getNotificationCooldownExpirationMap() {
+		return notificationCooldownExpirationMap;
+	}
+
+	public void setNotificationCooldownExpirationMap(Map<BigInteger, Map<String, Long>> notificationCooldownExpirationMap) {
+		this.notificationCooldownExpirationMap = notificationCooldownExpirationMap;
+	}
+
+	public Map<BigInteger, Map<String, Boolean>> getNotificationActiveStatusMap() {
+		return notificationActiveStatusMap;
+	}
+
+	public void setNotificationActiveStatusMap(Map<BigInteger, Map<String, Boolean>> notificationActiveStatusMap) {
+		this.notificationActiveStatusMap = notificationActiveStatusMap;
+	}
+	
+	public boolean isNotificationsCacheRefreshed() {
+		return isNotificationsCacheRefreshed;
+	}
+
+	public void setNotificationsCacheRefreshed(boolean isNotificationsCacheRefreshed) {
+		this.isNotificationsCacheRefreshed = isNotificationsCacheRefreshed;
+	}
+}
