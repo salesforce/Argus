e6fdb3fb60c0cc24978da7ff11d41df732b0717c
Justin Harringa <justin@harringa.com>
2018-06-19 16:26:03 -0700

    Source scripts from absolute path (${TRAVIS_BUILD_DIR}/script)

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # Generate & push code signing keys to keyserver.ubuntu.com, source into environment
-source .travis.gpg.sh
+source ${TRAVIS_BUILD_DIR}/.travis.gpg.sh
 
 # Utilize tokenized `.travis.settings.xml` to do deployment
-cp .travis.settings.xml $HOME/.m2/settings.xml
+cp ${TRAVIS_BUILD_DIR}/.travis.settings.xml ${HOME}/.m2/settings.xml
 
 # PROJECT_VERSION in the commands below should be incremented and sourced from project.version
-source project.version
+source ${TRAVIS_BUILD_DIR}/project.version
 
 # Set the version of all `pom.xml`s to ${PROJECT_VERSION} from the `project.version` file
+echo "Setting Maven version to ${PROJECT_VERSION}"
 mvn versions:set -DnewVersion=${PROJECT_VERSION}
 # Builds top level pom w/ zip
 mvn --batch-mode --non-recursive -DskipTests -DskipDockerBuild -U -Prelease -Dproject.version=${PROJECT_VERSION} clean install
