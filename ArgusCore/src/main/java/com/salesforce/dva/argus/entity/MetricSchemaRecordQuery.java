8a18c7cedc94ae8d3507c7ddf0f691ca9ea928bb
Naveen Reddy Karri <mail2naveen@gmail.com>
2018-07-16 11:28:50 -0700

    Fix2

--- ArgusCore/src/main/java/com/salesforce/dva/argus/entity/MetricSchemaRecordQuery.java
+++ ArgusCore/src/main/java/com/salesforce/dva/argus/entity/MetricSchemaRecordQuery.java
@@ -175,6 +175,7 @@ public class MetricSchemaRecordQuery extends SchemaQuery {
 
 	public boolean queryOnlyOnScope() {
 
+    	
 		if (Objects.equals(this.namespace, REGEX_MATCH_ANY) &&
 			Objects.equals(this.metric, REGEX_MATCH_ANY) &&
 			Objects.equals(this.tagKey, REGEX_MATCH_ANY) &&
