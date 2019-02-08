45b8dff6bd6ead7fe55c75e6176eec4eb3091d86
Jivko Dobrev <jdobrev@salesforce.com>
2019-02-08 15:25:26 -0800

    Remove cache-related eclipselink and jpa hints, disable cache
    @rev @nkarri
    
    Roles affected: WS and Alert

--- ArgusCore/src/main/java/com/salesforce/dva/argus/entity/DistributedSchedulingLock.java
+++ ArgusCore/src/main/java/com/salesforce/dva/argus/entity/DistributedSchedulingLock.java
@@ -49,8 +49,6 @@ import javax.persistence.Table;
 import javax.persistence.TypedQuery;
 import javax.persistence.Version;
 
-import org.eclipse.persistence.config.HintValues;
-import org.eclipse.persistence.config.QueryHints;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -126,7 +124,6 @@ public class DistributedSchedulingLock {
 		requireArgument(em != null, "Entity manager can not be null.");
 		
 		TypedQuery<DistributedSchedulingLock> query = em.createNamedQuery("DistributedSchedulingLock.GetEntryById", DistributedSchedulingLock.class);
-		query.setHint(QueryHints.REFRESH, HintValues.TRUE);
 		try {
 			query.setParameter("type", type);
 			return query.getSingleResult();
