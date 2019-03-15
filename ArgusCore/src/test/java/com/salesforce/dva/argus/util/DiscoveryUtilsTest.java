641e6373f4981e9d990c35d398f8ef04b521a028
Sundeep Tiyyagura <stiyyagura@salesforce.com>
2019-03-14 19:56:51 -0700

    W-5936651 - Fix the bug with 2M datapoint limit enforcement. Currently, we are not enforcing this limit if a query has a downsampler (#275)

---
+++ ArgusCore/src/test/java/com/salesforce/dva/argus/util/DiscoveryUtilsTest.java
@@ -0,0 +1,36 @@
+package com.salesforce.dva.argus.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.salesforce.dva.argus.AbstractTest;
+import com.salesforce.dva.argus.service.DiscoveryService;
+import com.salesforce.dva.argus.service.tsdb.MetricQuery;
+
+public class DiscoveryUtilsTest extends AbstractTest {
+
+    @Test
+    public void testGetMaxTimeSeriesAllowed() {
+        MetricQuery query = new MetricQuery("test","test",null,0L,0L);
+        query.setStartTimestamp(0L);
+        query.setEndTimestamp(60000l);
+        query.setDownsamplingPeriod(60000l);
+        assertEquals((long)DiscoveryService.maxTimeseriesAllowed(query), DiscoveryService.MAX_DATAPOINTS_PER_RESPONSE);
+        
+        query.setDownsamplingPeriod(30000l);
+        assertEquals((long)DiscoveryService.maxTimeseriesAllowed(query), DiscoveryService.MAX_DATAPOINTS_PER_RESPONSE/2);
+        
+        query.setDownsamplingPeriod(0l);
+        assertEquals((long)DiscoveryService.maxTimeseriesAllowed(query), DiscoveryService.MAX_DATAPOINTS_PER_RESPONSE);
+        
+        query.setDownsamplingPeriod(120000l);
+        assertEquals((long)DiscoveryService.maxTimeseriesAllowed(query), DiscoveryService.MAX_DATAPOINTS_PER_RESPONSE);
+        
+        query.setStartTimestamp(0L);
+        query.setEndTimestamp(480000l);
+        query.setDownsamplingPeriod(60000l);
+        assertEquals((long)DiscoveryService.maxTimeseriesAllowed(query), DiscoveryService.MAX_DATAPOINTS_PER_RESPONSE/8);
+        
+        query.setDownsamplingPeriod(2*60000l);
+        assertEquals((long)DiscoveryService.maxTimeseriesAllowed(query), DiscoveryService.MAX_DATAPOINTS_PER_RESPONSE/4);
+    }
+}
